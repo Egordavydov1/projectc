2274f82868796cafca1e2170311ba560901b40b6
Egordavydov1 <dsea4@mail.ru>
2024-09-20 16:08:06 +0300

    7-й коммит

--- main.c
+++ main.c
@@ -4,8 +4,8 @@ int main(void) {
     int a = 0;
     int b = 0;
     int c = 0;
-    scanf("%d%d/n",&a,&b);
-    printf("%d",a - b);
+    scanf("%d%d%d/n",&a,&b,&c);
+    printf("%d",a - b + c);
     return 0;
 }
 
