211b1a26a16dc00a06113fd6cbc28eac165cd20e
Egordavydov1 <dsea4@mail.ru>
2024-09-20 16:50:40 +0300

    C1

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main(void) {
-    printf("input six number/n");
+    printf("input seven number/n");
     int a = 0;
     int b = 0;
     int c = 0;
     int w = 0;
     int z = 0;
     int r = 0;
-    scanf("%d%d%d%d%d%d%d/n",&a,&b,&c,&w,&z,&r);
-    printf("%d",a - b + c + w - z + r);
+    int u = 0;
+    scanf("%d%d%d%d%d%d%d%d/n",&a,&b,&c,&w,&z,&r,&u);
+    printf("%d",a - b + c + w - z + r - u);
     return 0;
 }
 
