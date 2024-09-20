ab8a4d7ea9363c7cdc3e23aeece52b6a5ef1131e
Egordavydov1 <dsea4@mail.ru>
2024-09-20 17:32:17 +0300

    C2

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ int main(void) {
     int e = 0;
     int q = 0;
     int j = 0;
+    int d = 0;
     scanf("%d%d%d%d%d%d%d%d%d/n",&a,&b,&c,&w,&z,&r,&u,&e);
     printf("%d",a - b + c + w - z + r - u + e);
     return 0;
